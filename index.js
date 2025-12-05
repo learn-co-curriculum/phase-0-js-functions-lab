07440c26e43b245c64631279977757b1df5a5587
George Achieng <george.achieng@student.moringaschool.com>
2025-12-05 13:06:35 +0300

    These functions work fully

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+function calculateTax(amount)
+   {console.log(amount * 0.1);}
+
+function convertToUpperCase(text)
+    {console.log(text.toUpperCase());}
+   
+function findMaximum(num1,num2){
+    if (num1 > num2) 
+        {console.log(num1);}
+    else if (num2 > num1)
+        {console.log(num2);}
+    else {console.log("Both numbers are equal");}}
+
+function isPalindrome(word)
+    {console.log(word === word.split('').reverse().join(''));}
+
+
+function calculateDiscountedPrice(originalPrice,discountPercentage)
+    {console.log(originalPrice - (originalPrice * discountPercentage));}
+
+    
